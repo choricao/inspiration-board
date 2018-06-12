a51e012c18b3bc64154701291b8755bae8c6cb11
Zheng Cao <chori84@gmail.com>
2018-06-12 16:00:14 -0700

    Implement shallow snapshot test for App

--- src/App.test.js
+++ src/App.test.js
@@ -5,10 +5,10 @@ import App from './App';
 
 describe('App', () => {
 
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  test('that it renders App with shallow rendering', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).toMatchSnapshot();
+    wrapper.unmount();
   });
 
 });
